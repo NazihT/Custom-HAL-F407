d0031e0114cf707707c84cc02091d1d882d0e3b6
Nazih <nazihtirellil5@gmail.com>
2026-04-02 00:18:35 +0100

    Update RCC

--- include/TIMER_HAL.h
+++ include/TIMER_HAL.h
@@ -1,56 +1,65 @@
+#ifndef TIMER_HAL_H
+#define TIMER_HAL_H
+
 #include "stm32f407xx.h"
 
-typedef enum{
+/* ---- assert_param ---- */
+#ifdef USE_FULL_ASSERT
+  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+  void assert_failed(uint8_t *file, uint32_t line);
+#else
+  #define assert_param(expr) ((void)0U)
+#endif
+
+/* ---- validation macros ---- */
+#define IS_TIM(tim)           ((tim) == TIM2 || (tim) == TIM3 || (tim) == TIM4 || \
+                               (tim) == TIM5 || (tim) == TIM6 || (tim) == TIM7)
+#define IS_PWM_CHANNEL(ch)    ((ch) <= CHANNEL_4)
+#define IS_DUTY_PERCENT(pct)  ((pct) <= 100)
+#define IS_TIM_MODE(m)        ((m) == TIMER_MODE_BASIC || (m) == TIMER_MODE_PWM)
+#define IS_INTR_STATE(s)      ((s) == TIMER_INTERRUPT_ENABLE || (s) == TIMER_INTERRUPT_DISABLE)
+
+typedef enum {
     TIMER_INTERRUPT_ENABLE,
     TIMER_INTERRUPT_DISABLE
-
-}Interrupt_State;
+} Interrupt_State;
 
 typedef enum {
     CHANNEL_1,
     CHANNEL_2,
     CHANNEL_3,
     CHANNEL_4,
+} PWM_Channel;
 
-}PWM_Channel;
 typedef enum {
     TIMER_MODE_BASIC,
     TIMER_MODE_PWM
 } Timer_MODE;
 
-
 typedef struct {
-
-    Timer_MODE  Mode;
+    Timer_MODE      Mode;
     Interrupt_State InterruptState;
-    uint16_t PSC;
-    uint16_t ARR;
-    uint8_t DefaultDuty;
-    PWM_Channel CHx;
-
+    uint16_t        PSC;
+    uint16_t        ARR;
+    uint8_t         DefaultDuty_Percent;
+    PWM_Channel     CHx;
 } TIMER_Config_t;
 
+void       TIM_Init(TIM_TypeDef *TIMx, TIMER_Config_t *cfg);
+void       TIM_EnableClock(TIM_TypeDef *TIMx);
+void       TIM_Start(TIM_TypeDef *TIMx);
+void       TIM_Stop(TIM_TypeDef *TIMx);
+void       TIM_EnableInterrupt(TIM_TypeDef *TIMx, Interrupt_State intr);
+void       TIM_PWM_EnableChannel(TIM_TypeDef *TIMx, PWM_Channel channel);
+void       TIM_PWM_SetDutyCycle(TIM_TypeDef *TIMx, PWM_Channel channel, uint8_t duty_percent);
+uint8_t    TIM_GetFlagStatus(TIM_TypeDef *TIMx);
+void       TIM_ClearFlag(TIM_TypeDef *TIMx);
+void       TIM_ResetCounter(TIM_TypeDef *TIMx);
+uint32_t   TIM_GetCounter(TIM_TypeDef *TIMx);
+void       TIM_SetCounter(TIM_TypeDef *TIMx, uint32_t value);
+void       TIM_SetPrescaler(TIM_TypeDef *TIMx, uint16_t prescaler);
+void       TIM_SetAutoReload(TIM_TypeDef *TIMx, uint32_t auto_reload);
+void       TIM_OnePulseMode(TIM_TypeDef *TIMx);
+IRQn_Type  TIM_GetIRQn(TIM_TypeDef *TIMx);
 
-void TIM_Init(TIM_TypeDef *TIMx , TIMER_Config_t *ConfigStruct );
-
-
-
-
-
-void TIM_PWM_EnableChannel(TIM_TypeDef *TIMx, PWM_Channel Channel);
-void TIM_PWM_SetDutyCycle(TIM_TypeDef *TIMx , PWM_Channel Channel, uint16_t DutyCycle);
-
-void TIM_EnableClock(TIM_TypeDef *TIMx);
-void TIM_Start(TIM_TypeDef *TIMx);
-void TIM_Stop(TIM_TypeDef *TIMx);
-IRQn_Type TIM_GetIRQn(TIM_TypeDef *TIMx);
-void TIM_EnableInterrupt(TIM_TypeDef *TIMx , Interrupt_State Intr  );
-uint8_t TIM_GetFlagStatus(TIM_TypeDef *TIMx) ;
-void TIM_ClearFlag(TIM_TypeDef *TIMx) ;
-void TIM_ResetCounter(TIM_TypeDef *TIMx);
-uint32_t TIM_GetCounter(TIM_TypeDef *TIMx);
-void TIM_SetCounter(TIM_TypeDef *TIMx, uint32_t value);
-void TIM_SetPrescaler(TIM_TypeDef *TIMx, uint16_t prescaler);
-void TIM_SetAutoReload(TIM_TypeDef *TIMx, uint32_t auto_reload);
-void TIM_OnePulseMode(TIM_TypeDef *TIMx);
-
+#endif
--- src/TIMER.HAL.c
+++ src/TIMER.HAL.c
@@ -1,197 +1,172 @@
 #include "TIMER_HAL.h"
 
-void TIM_Init(TIM_TypeDef *TIMx , TIMER_Config_t *ConfigStruct )
+__attribute__((weak)) void assert_failed(uint8_t *file, uint32_t line)
 {
-    if(ConfigStruct->Mode==TIMER_MODE_BASIC)
-    {
-        
-      TIM_EnableClock(TIMx);
-      TIMx->PSC = ConfigStruct->PSC;
-      TIMx->ARR = ConfigStruct->ARR;
-      TIM_EnableInterrupt(TIMx,ConfigStruct->InterruptState);
-    }
-    else if (ConfigStruct->Mode==TIMER_MODE_PWM)
-    {
-          TIM_EnableClock(TIMx);
-          TIMx->PSC= ConfigStruct ->PSC;
-          TIMx->ARR= ConfigStruct ->ARR;
-          TIM_PWM_SetDutyCycle(TIMx,ConfigStruct->CHx,ConfigStruct->DefaultDuty);
-          TIM_PWM_EnableChannel(TIMx,ConfigStruct->CHx);
-    }
+    (void)file;
+    (void)line;
+
+    RCC->AHB1ENR |= (1 << 3);
+    GPIOD->MODER |= (1 << 28);
+    GPIOD->BSRR   = (1 << 14);
+
+    while(1);
 }
 
 
-void TIM_PWM_EnableChannel(TIM_TypeDef *TIMx, PWM_Channel Channel)
+void TIM_EnableInterrupt(TIM_TypeDef *TIMx, Interrupt_State intr)
 {
-    if (Channel == CHANNEL_1)
-    {
-        TIMx->CCMR1 &=~(7<<4); 
-        TIMx->CCMR1 |=(6<<4);
-        TIMx->CCMR1 |=(1<<3);
-        TIMx->CCER |=(1<<0);
+    assert_param(IS_TIM(TIMx));
+    assert_param(IS_INTR_STATE(intr));
 
-
-    }
-    else if (Channel==CHANNEL_2)
-    {
-        TIMx->CCMR1 &=~(7<<12); 
-        TIMx->CCMR1 |=(6<<12);
-        TIMx->CCMR1 |=(1<<11);
-        TIMx->CCER |=(1<<4);
-    }
-     else if (Channel==CHANNEL_3)
-    {
-        TIMx->CCMR2 &=~(7<<4); 
-        TIMx->CCMR2 |=(6<<4);
-        TIMx->CCMR2 |=(1<<3);
-        TIMx->CCER |=(1<<8);
-
-    }
-     else if (Channel==CHANNEL_4)
-    {
-        TIMx->CCMR2 &=~(7<<12); 
-        TIMx->CCMR2 |=(6<<12);
-        TIMx->CCMR2 |=(1<<11);
-        TIMx->CCER |=(1<<12);
-
-    }
-    TIMx->CR1 |= (1<<7);
-
-
-}
-
-
-void TIM_PWM_SetDutyCycle(TIM_TypeDef *TIMx , PWM_Channel Channel, uint16_t DutyCycle)
-{
-    if (Channel==CHANNEL_1)
-    {
-      TIMx->CCR1 = (TIMx->ARR * DutyCycle)/100;
-    }
-    else if (Channel==CHANNEL_2)
-    {
-       TIMx->CCR2 = (TIMx->ARR * DutyCycle)/100;
-  
-    }
-    else if (Channel==CHANNEL_3)
-    {
-       TIMx->CCR3 = (TIMx->ARR * DutyCycle)/100;
-    }
-    else if (Channel==CHANNEL_4)
-    {
-        TIMx->CCR4 = (TIMx->ARR * DutyCycle)/100;
- 
-    }
-
-}
-void TIM_EnableInterrupt(TIM_TypeDef *TIMx , Interrupt_State Intr  )
-{
-   uint8_t IRQnumber=TIM_GetIRQn(TIMx);
-
-   if (Intr==TIMER_INTERRUPT_ENABLE)
-    {
-        TIMx->DIER |=(1<<0);
-        TIMx->SR &=~(1<<0);
-        NVIC_EnableIRQ(IRQnumber);
-        
-    }
-   else 
-    {
-        TIMx->DIER &=~ (1<<0);
-        NVIC_DisableIRQ(IRQnumber);
+    if (intr == TIMER_INTERRUPT_ENABLE) {
+        TIMx->DIER |=  (1 << 0);
+        TIMx->SR   &= ~(1 << 0);
+        NVIC_EnableIRQ(TIM_GetIRQn(TIMx));
+    } else {
+        TIMx->DIER &= ~(1 << 0);
+        NVIC_DisableIRQ(TIM_GetIRQn(TIMx));
     }
 }
 
+void TIM_PWM_EnableChannel(TIM_TypeDef *TIMx, PWM_Channel channel)
+{
+    assert_param(IS_TIM(TIMx));
+    assert_param(IS_PWM_CHANNEL(channel));
 
+    if (channel == CHANNEL_1) {
+        TIMx->CCMR1 &= ~(7 << 4);
+        TIMx->CCMR1 |=  (6 << 4) | (1 << 3);
+        TIMx->CCER  |=  (1 << 0);
+    } else if (channel == CHANNEL_2) {
+        TIMx->CCMR1 &= ~(7 << 12);
+        TIMx->CCMR1 |=  (6 << 12) | (1 << 11);
+        TIMx->CCER  |=  (1 << 4);
+    } else if (channel == CHANNEL_3) {
+        TIMx->CCMR2 &= ~(7 << 4);
+        TIMx->CCMR2 |=  (6 << 4) | (1 << 3);
+        TIMx->CCER  |=  (1 << 8);
+    } else {
+        TIMx->CCMR2 &= ~(7 << 12);
+        TIMx->CCMR2 |=  (6 << 12) | (1 << 11);
+        TIMx->CCER  |=  (1 << 12);
+    }
+
+    TIMx->CR1 |= (1 << 7);
+}
+
+void TIM_PWM_SetDutyCycle(TIM_TypeDef *TIMx, PWM_Channel channel, uint8_t duty_percent)
+{
+    assert_param(IS_TIM(TIMx));
+    assert_param(IS_PWM_CHANNEL(channel));
+    assert_param(IS_DUTY_PERCENT(duty_percent));
+
+    if      (channel == CHANNEL_1) TIMx->CCR1 = (TIMx->ARR * duty_percent) / 100;
+    else if (channel == CHANNEL_2) TIMx->CCR2 = (TIMx->ARR * duty_percent) / 100;
+    else if (channel == CHANNEL_3) TIMx->CCR3 = (TIMx->ARR * duty_percent) / 100;
+    else                           TIMx->CCR4 = (TIMx->ARR * duty_percent) / 100;
+}
+
+void TIM_Init(TIM_TypeDef *TIMx, TIMER_Config_t *cfg)
+{
+    assert_param(IS_TIM(TIMx));
+    assert_param(cfg != NULL);
+    assert_param(IS_TIM_MODE(cfg->Mode));
+
+    TIM_EnableClock(TIMx);
+    TIMx->PSC = cfg->PSC;
+    TIMx->ARR = cfg->ARR;
+
+    if (cfg->Mode == TIMER_MODE_BASIC) {
+        TIM_EnableInterrupt(TIMx, cfg->InterruptState);
+    } else {
+        assert_param(IS_PWM_CHANNEL(cfg->CHx));
+        assert_param(IS_DUTY_PERCENT(cfg->DefaultDuty_Percent));
+        TIM_PWM_SetDutyCycle(TIMx, cfg->CHx, cfg->DefaultDuty_Percent);
+        TIM_PWM_EnableChannel(TIMx, cfg->CHx);
+    }
+}
 
 void TIM_Start(TIM_TypeDef *TIMx)
 {
-    TIMx->CNT=0;
-    TIMx->CR1 |=(1<<0);
-}
-void TIM_Stop(TIM_TypeDef *TIMx)
-{
-    TIMx->CNT=0;
-    TIMx->CR1 &=~(1<<0);
+    assert_param(IS_TIM(TIMx));
+    TIMx->CNT = 0;
+    TIMx->CR1 |= (1 << 0);
 }
 
-uint8_t TIM_GetFlagStatus(TIM_TypeDef *TIMx) {
+void TIM_Stop(TIM_TypeDef *TIMx)
+{
+    assert_param(IS_TIM(TIMx));
+    TIMx->CNT = 0;
+    TIMx->CR1 &= ~(1 << 0);
+}
+
+uint8_t TIM_GetFlagStatus(TIM_TypeDef *TIMx)
+{
+    assert_param(IS_TIM(TIMx));
     return (TIMx->SR & (1 << 0)) ? 1 : 0;
 }
-void TIM_ClearFlag(TIM_TypeDef *TIMx) 
+
+void TIM_ClearFlag(TIM_TypeDef *TIMx)
 {
-    TIMx->SR &=~(1<<0);
+    assert_param(IS_TIM(TIMx));
+    TIMx->SR &= ~(1 << 0);
 }
+
 void TIM_ResetCounter(TIM_TypeDef *TIMx)
 {
-    TIMx->CNT=0;
+    assert_param(IS_TIM(TIMx));
+    TIMx->CNT = 0;
 }
 
 uint32_t TIM_GetCounter(TIM_TypeDef *TIMx)
 {
-    return(TIMx->CNT);
+    assert_param(IS_TIM(TIMx));
+    return TIMx->CNT;
 }
+
 void TIM_SetCounter(TIM_TypeDef *TIMx, uint32_t value)
 {
-    TIMx->CNT=value;
+    assert_param(IS_TIM(TIMx));
+    TIMx->CNT = value;
 }
+
 void TIM_SetPrescaler(TIM_TypeDef *TIMx, uint16_t prescaler)
 {
+    assert_param(IS_TIM(TIMx));
     TIMx->PSC = prescaler;
 }
+
 void TIM_SetAutoReload(TIM_TypeDef *TIMx, uint32_t auto_reload)
 {
-    TIMx->ARR=auto_reload;
+    assert_param(IS_TIM(TIMx));
+    TIMx->ARR = auto_reload;
 }
+
 void TIM_OnePulseMode(TIM_TypeDef *TIMx)
 {
-    TIMx->CR1 |=(1<<3);
+    assert_param(IS_TIM(TIMx));
+    TIMx->CR1 |= (1 << 3);
 }
-
-
 void TIM_EnableClock(TIM_TypeDef *TIMx)
 {
-   if (TIMx == TIM2) RCC->APB1ENR |= (1 << 0);
+    assert_param(IS_TIM(TIMx));
+
+    if      (TIMx == TIM2) RCC->APB1ENR |= (1 << 0);
     else if (TIMx == TIM3) RCC->APB1ENR |= (1 << 1);
     else if (TIMx == TIM4) RCC->APB1ENR |= (1 << 2);
     else if (TIMx == TIM5) RCC->APB1ENR |= (1 << 3);
     else if (TIMx == TIM6) RCC->APB1ENR |= (1 << 4);
     else if (TIMx == TIM7) RCC->APB1ENR |= (1 << 5);
-    
-
 }
+
 IRQn_Type TIM_GetIRQn(TIM_TypeDef *TIMx)
 {
-    if (TIMx == TIM2) return TIM2_IRQn;
+    assert_param(IS_TIM(TIMx));
+
+    if      (TIMx == TIM2) return TIM2_IRQn;
     else if (TIMx == TIM3) return TIM3_IRQn;
     else if (TIMx == TIM4) return TIM4_IRQn;
     else if (TIMx == TIM5) return TIM5_IRQn;
     else if (TIMx == TIM6) return TIM6_DAC_IRQn;
-    else if (TIMx == TIM7) return TIM7_IRQn;
-  
-    else  while(1);
-   
-
+    else                   return TIM7_IRQn;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
