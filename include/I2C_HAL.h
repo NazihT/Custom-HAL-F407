c9468c9069f5a92dcb85d59aa033181a97d1e011
Nazih <nazihtirellil5@gmail.com>
2026-04-23 01:38:05 +0100

    fixed fast mode TRISE and added parameter insertion

--- include/I2C_HAL.h
+++ include/I2C_HAL.h
@@ -1,36 +1,41 @@
+#ifndef I2C_HAL_H
+#define I2C_HAL_H
+
 #include "stm32f407xx.h"
 
+/* ---- assert_param ---- */
+#ifdef USE_FULL_ASSERT
+  #define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+  void assert_failed(uint8_t *file, uint32_t line);
+#else
+  #define assert_param(expr) ((void)0U)
+#endif
 
-typedef enum
-{
-    I2C_Standard_Mode,
-    I2C_Fast_Mode
+/* ---- validation macros ---- */
+#define IS_I2C(i2c)        ((i2c) == I2C1 || (i2c) == I2C2 || (i2c) == I2C3)
+#define IS_I2C_SPEED(s)    ((s) == I2C_STANDARD_MODE || (s) == I2C_FAST_MODE)
 
-}i2c_Speed;
+#define PCLK1_FREQ         16000000U
 
+typedef enum {
+    I2C_STANDARD_MODE,
+    I2C_FAST_MODE,
+} I2C_Speed;
 
-typedef struct
-{
-    i2c_Speed  speed_mode;
-    uint16_t   frequency;
+typedef struct {
+    I2C_Speed speed_mode;
+    uint16_t  frequency;       // in kHz  (e.g. 100 = 100kHz, 400 = 400kHz)
+} I2C_Config_t;
 
-} I2C_Config_t ;
-
-typedef enum
-{
+typedef enum {
     NACK,
-    ACK
-
+    ACK,
 } ACK_Status;
 
+void       I2C_Init(I2C_TypeDef *i2cx, I2C_Config_t *cfg);
+void       I2C_EnableClock(I2C_TypeDef *i2cx);
+void       I2C_SetSpeed(I2C_TypeDef *i2cx, I2C_Speed speed_mode, uint16_t frequency);
+ACK_Status I2C_Transmit(I2C_TypeDef *i2cx, uint8_t addr, uint8_t *data, uint8_t size);
+ACK_Status I2C_Receive(I2C_TypeDef *i2cx, uint8_t addr, uint8_t size, uint8_t *buffer);
 
-void I2C_Init(I2C_TypeDef *i2cx , I2C_Config_t *Config );
-ACK_Status I2C_Transmit(I2C_TypeDef *i2cx , uint8_t addr , uint8_t *data , uint8_t size);
-void I2C_SetSpeed(I2C_TypeDef *i2cx , i2c_Speed Speed , uint16_t frequency);
-uint16_t get_CCR_SM(uint32_t pclk, uint32_t scl_freq) ;
-uint16_t get_CCR_FM(uint32_t pclk, uint32_t scl_freq) ;
-
-ACK_Status I2C_Recieve(I2C_TypeDef* i2cx, uint8_t addr , uint8_t size , uint8_t* buffer);
-
-
-void I2C_EnableClock(I2C_TypeDef *i2cx);
+#endif
--- src/I2C_HAL.c
+++ src/I2C_HAL.c
@@ -1,204 +1,178 @@
 #include "I2C_HAL.h"
-#include "RCC_HAL.h"
 
+/* ---- internal helpers ---- */
+static uint16_t I2C_CalcCCR_SM(uint32_t pclk, uint32_t scl_khz)
+{
+    return (uint16_t)(pclk / (2U * scl_khz * 1000U));
+}
 
-void I2C_Init(I2C_TypeDef *i2cx , I2C_Config_t *Config )
-{  I2C_EnableClock(i2cx);
-    I2C_SetSpeed(i2cx,Config->speed_mode,Config->frequency);
-        i2cx->TRISE = (PCLK1_FREQ / 1000000) + 1;
+static uint16_t I2C_CalcCCR_FM(uint32_t pclk, uint32_t scl_khz)
+{
+    return (uint16_t)(pclk / (3U * scl_khz * 1000U));
+}
 
-    i2cx->CR1 |=(1<<I2C_CR1_PE_Pos);
-    
+/* ------------------------------------------------------------------ */
+
+void I2C_EnableClock(I2C_TypeDef *i2cx)
+{
+    assert_param(IS_I2C(i2cx));
+
+    if      (i2cx == I2C1) RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C1EN_Pos);
+    else if (i2cx == I2C2) RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C2EN_Pos);
+    else                   RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C3EN_Pos);
+}
+
+void I2C_SetSpeed(I2C_TypeDef *i2cx, I2C_Speed speed_mode, uint16_t frequency)
+{
+    assert_param(IS_I2C(i2cx));
+    assert_param(IS_I2C_SPEED(speed_mode));
+
+    i2cx->CCR &= ~(0xFFF);
+
+    if (speed_mode == I2C_STANDARD_MODE) {
+        i2cx->CCR &= ~(1 << I2C_CCR_FS_Pos);
+        i2cx->CCR |=  I2C_CalcCCR_SM(PCLK1_FREQ, frequency);
+        i2cx->TRISE = (PCLK1_FREQ / 1000000U) + 1U;            // Trise = 1000ns
+    } else {
+        i2cx->CCR |= (1 << I2C_CCR_FS_Pos);
+        i2cx->CCR |=  I2C_CalcCCR_FM(PCLK1_FREQ, frequency);
+        i2cx->TRISE = ((PCLK1_FREQ / 1000000U) * 3U) / 10U + 1U; // Trise = 300ns
+    }
+}
+
+void I2C_Init(I2C_TypeDef *i2cx, I2C_Config_t *cfg)
+{
+    assert_param(IS_I2C(i2cx));
+    assert_param(cfg != NULL);
+    assert_param(IS_I2C_SPEED(cfg->speed_mode));
+
+    I2C_EnableClock(i2cx);
+
+    // CR2 FREQ must be set to APB1 clock in MHz before touching CCR/TRISE
+    i2cx->CR2 &= ~(0x3F);
+    i2cx->CR2 |=  (PCLK1_FREQ / 1000000U);
+
+    I2C_SetSpeed(i2cx, cfg->speed_mode, cfg->frequency);
+
+    i2cx->CR1 |= (1 << I2C_CR1_PE_Pos);
 }
 
 ACK_Status I2C_Transmit(I2C_TypeDef *i2cx, uint8_t addr, uint8_t *data, uint8_t size)
 {
+    assert_param(IS_I2C(i2cx));
+
     uint32_t timeout;
-    
-    // Wait for bus not busy
-    timeout = 160000;
+
+    timeout = 160000U;
     while ((i2cx->SR2 & I2C_SR2_BUSY) && timeout--);
     if (!timeout) return NACK;
-    
+
     i2cx->CR1 |= I2C_CR1_START;
-    
-    // Wait for Start Bit
-    timeout = 160000;
+
+    timeout = 160000U;
     while (!(i2cx->SR1 & I2C_SR1_SB) && timeout--);
     if (!timeout) return NACK;
-    
-    i2cx->DR = (addr << 1) | 0; // write
-    
-    timeout = 160000;
+
+    i2cx->DR = (uint8_t)((addr << 1) | 0x00);   // write
+
+    timeout = 160000U;
     while (!(i2cx->SR1 & I2C_SR1_ADDR) && !(i2cx->SR1 & I2C_SR1_AF) && timeout--);
-    
-    if (i2cx->SR1 & I2C_SR1_AF)
-    {
-        (void)i2cx->SR1;
-        i2cx->SR1 &= ~I2C_SR1_AF;  // Clear AF
+
+    if (i2cx->SR1 & I2C_SR1_AF) {
+        i2cx->SR1  &= ~I2C_SR1_AF;
+        i2cx->CR1  |=  I2C_CR1_STOP;
+        return NACK;
+    }
+    if (!timeout) {
         i2cx->CR1 |= I2C_CR1_STOP;
         return NACK;
     }
-    
-    if (!timeout)
-    {
-        i2cx->CR1 |= I2C_CR1_STOP;
-        return NACK;
-    }
-    
-    // Clear ADDR flag
+
+    // clear ADDR
     (void)i2cx->SR1;
     (void)i2cx->SR2;
-    
-    // Special case: size = 0 (just address check)
-    if (size == 0)
-    {
+
+    if (size == 0) {
         i2cx->CR1 |= I2C_CR1_STOP;
         return ACK;
     }
-    
-    // Send data bytes
-    for (uint8_t i = 0; i < size; i++)
-    {
-        while (!(i2cx->SR1 & I2C_SR1_TXE) && 
-               !(i2cx->SR1 & I2C_SR1_AF));
-        
-        if (i2cx->SR1 & I2C_SR1_AF)
-        {
-            (void)i2cx->SR1;
+
+    for (uint8_t i = 0; i < size; i++) {
+        while (!(i2cx->SR1 & I2C_SR1_TXE) && !(i2cx->SR1 & I2C_SR1_AF));
+
+        if (i2cx->SR1 & I2C_SR1_AF) {
             i2cx->SR1 &= ~I2C_SR1_AF;
-            i2cx->CR1 |= I2C_CR1_STOP;
+            i2cx->CR1 |=  I2C_CR1_STOP;
             return NACK;
         }
-        
+
         i2cx->DR = data[i];
     }
-    
-    // Wait for last byte transmission complete
-    while (!(i2cx->SR1 & I2C_SR1_BTF) && 
-           !(i2cx->SR1 & I2C_SR1_AF));
-    
-    if (i2cx->SR1 & I2C_SR1_AF)
-    {
-        (void)i2cx->SR1;
+
+    while (!(i2cx->SR1 & I2C_SR1_BTF) && !(i2cx->SR1 & I2C_SR1_AF));
+
+    if (i2cx->SR1 & I2C_SR1_AF) {
         i2cx->SR1 &= ~I2C_SR1_AF;
-        i2cx->CR1 |= I2C_CR1_STOP;
+        i2cx->CR1 |=  I2C_CR1_STOP;
         return NACK;
     }
-    
+
     i2cx->CR1 |= I2C_CR1_STOP;
     return ACK;
 }
 
-ACK_Status I2C_Receive(I2C_TypeDef* i2cx, uint8_t addr, uint8_t size, uint8_t* buffer)
+ACK_Status I2C_Receive(I2C_TypeDef *i2cx, uint8_t addr, uint8_t size, uint8_t *buffer)
 {
+    assert_param(IS_I2C(i2cx));
+    assert_param(buffer != NULL);
+
     uint32_t timeout;
-    
-    // Wait for bus not busy
-    timeout = 160000;
+
+    timeout = 160000U;
     while ((i2cx->SR2 & I2C_SR2_BUSY) && timeout--);
     if (!timeout) return NACK;
-    
+
     i2cx->CR1 |= I2C_CR1_START;
-    
-    // Wait for Start Bit
-    timeout = 160000;
+
+    timeout = 160000U;
     while (!(i2cx->SR1 & I2C_SR1_SB) && timeout--);
     if (!timeout) return NACK;
-    
-    i2cx->DR = (addr << 1) | 0x1; // read
-    
-    // Wait for ADDR or AF
-    timeout = 160000;
-    while (!(i2cx->SR1 & I2C_SR1_ADDR) && 
-           !(i2cx->SR1 & I2C_SR1_AF) && 
-           timeout--);
-    
-    if (i2cx->SR1 & I2C_SR1_AF)
-    {
-        (void)i2cx->SR1;
+
+    i2cx->DR = (uint8_t)((addr << 1) | 0x01);   // read
+
+    timeout = 160000U;
+    while (!(i2cx->SR1 & I2C_SR1_ADDR) && !(i2cx->SR1 & I2C_SR1_AF) && timeout--);
+
+    if (i2cx->SR1 & I2C_SR1_AF) {
         i2cx->SR1 &= ~I2C_SR1_AF;
+        i2cx->CR1 |=  I2C_CR1_STOP;
+        return NACK;
+    }
+    if (!timeout) {
         i2cx->CR1 |= I2C_CR1_STOP;
         return NACK;
     }
-    
-    if (!timeout)
-    {
-        i2cx->CR1 |= I2C_CR1_STOP;
-        return NACK;
-    }
-    
-    // Clear ADDR flag
+
+    // clear ADDR
     (void)i2cx->SR1;
     (void)i2cx->SR2;
+
     i2cx->CR1 |= I2C_CR1_ACK;
 
-    
-    // Receive bytes
-    for (int index = 0; index < size; index++)
-    {
-        // Set NACK on last byte
-        if (index == size - 1)
-        {
-            i2cx->CR1 &= ~I2C_CR1_ACK;  // NACK for last byte
-        }
-        
-        // Wait for data with FRESH timeout
-        timeout = 160000;
+    for (uint8_t i = 0; i < size; i++) {
+        if (i == size - 1)
+            i2cx->CR1 &= ~I2C_CR1_ACK;     // NACK before reading last byte
+
+        timeout = 160000U;
         while (!(i2cx->SR1 & I2C_SR1_RXNE) && timeout--);
-        
-        if (!timeout) 
-        {
+        if (!timeout) {
             i2cx->CR1 |= I2C_CR1_STOP;
             return NACK;
         }
-        
-        buffer[index] = i2cx->DR;
+
+        buffer[i] = (uint8_t)i2cx->DR;
     }
-    
+
     i2cx->CR1 |= I2C_CR1_STOP;
     return ACK;
-}
-
-
-void I2C_SetSpeed(I2C_TypeDef *i2cx , i2c_Speed Speed_Mode , uint16_t frequency)
-{
-    if (Speed_Mode == I2C_Standard_Mode)
-    {
-        uint16_t CCR_value = get_CCR_SM(PCLK1_FREQ, frequency);
-        i2cx->CCR &= ~(1 << I2C_CCR_FS_Pos);
-        i2cx->CCR &= ~(0xFFF);
-        i2cx->CCR |= (CCR_value << 0);
-    }
-    else if (Speed_Mode == I2C_Fast_Mode)
-    {
-        uint16_t CCR_value = get_CCR_FM(PCLK1_FREQ, frequency);
-        i2cx->CCR |= (1 << I2C_CCR_FS_Pos);
-        i2cx->CCR &= ~(0xFFF);
-        i2cx->CCR |= (CCR_value << 0);
-    }
-}
-
-uint16_t get_CCR_SM(uint32_t pclk, uint32_t scl_freq) {
-    return pclk / (2 * scl_freq * 1000); 
-}
-
-uint16_t get_CCR_FM(uint32_t pclk, uint32_t scl_freq) {
-    return pclk / (3 * scl_freq * 1000); 
-}
-
-void I2C_EnableClock(I2C_TypeDef *i2cx)
-{
-    if (i2cx == I2C1)
-    {
-        RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C1EN_Pos);
-    }
-    if (i2cx == I2C2)
-    {
-        RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C2EN_Pos);
-    }
-    if (i2cx == I2C3)
-    {
-        RCC->APB1ENR |= (1 << RCC_APB1ENR_I2C3EN_Pos);
-    }
 }
